ce788b39e1d90526bb128a2740f683015677fc67
Alexis CHEMEL <chemel@users.noreply.github.com>
2016-11-09 13:49:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # domq
 Command line HTML DOM Parser
+
+Install:
+======
+
+```bash
+
+composer install
+
+# Create symlink (optional)
+ln -s /var/www/domq/domq.php /usr/bin/domq
+
+```
+
+Usage:
+======
+
+```bash
+
+# Query Hacker News Titles
+php domq.php https://news.ycombinator.com/ a.storylink innertext
+
+# Query Hacker News Urls
+php domq.php https://news.ycombinator.com/ a.storylink getAttribute href
+
+# Query Google Urls
+php domq.php https://www.google.fr/search?q=github 'h3.r a' getAttribute href
+
+# Show help
+php domq.php --help
+
+```
---
+++ domq.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Commando\Command;
+use Alc\HtmlDomParserHelper;
+
+$command = new Command();
+
+// Define first option
+$command->option()
+    ->require()
+    ->describedAs('url');
+
+$command->option()
+    ->require()
+    ->describedAs('selector');
+
+$command->option()
+    ->require()
+    ->describedAs('function');
+
+$command->option()
+    ->describedAs('arguments');
+
+// Arguments
+$url = $command[0];
+$selector = $command[1];
+$function = $command[2];
+$arguments = isset($command[3]) ? array($command[3]) : array();
+
+// Scrappe
+$helper = new HtmlDomParserHelper();
+$parser = $helper->parse($url);
+
+$nodes = $parser->find($selector);
+
+if( !empty($nodes) ) {
+
+	foreach( $nodes as $node ) {
+
+		echo call_user_func_array(array($node, $function), $arguments), "\n";
+	}
+}
+
+$helper->clear();
